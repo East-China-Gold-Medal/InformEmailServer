31dc48d0e8e00ae977ad2935b77ccd65f5c39246
CaYyon Tsai <c764994628@gmail.com>
2024-06-25 13:30:37 +0800

    fix: fix file reading error

--- src/main/java/com/EastChinaGoldMedal/utils/HtmlGenerator.java
+++ src/main/java/com/EastChinaGoldMedal/utils/HtmlGenerator.java
@@ -1,20 +1,17 @@
 package com.EastChinaGoldMedal.utils;
 
+import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.File;
 import java.io.IOException;
 
 @Slf4j
 public class HtmlGenerator {
+    private static final String EMAIL_HTML_TEMPLATE  = "templates/HtmlEmailTemplates/temp3.html";
+
     public static String loadHtml(String content) throws IOException {
-        File input = new File("src/main/resources/templates/HtmlEmailTemplates/temp1.html");
-        Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
-        String message = doc.outerHtml();
-        String result = StrUtil.format(message, content);
+        String templateContent = ResourceUtil.readUtf8Str(EMAIL_HTML_TEMPLATE);
+        String result = StrUtil.format(templateContent, content);
         return result;
     }
 
